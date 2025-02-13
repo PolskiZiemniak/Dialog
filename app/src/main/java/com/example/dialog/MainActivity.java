39c31282bbf26751c56d9db52aac6e9f17f253cf
PolskiZiemniak <polskiziemniak2018@gmail.com>
2025-02-13 17:02:07 +0100

    helo

--- app/src/main/java/com/example/dialog/MainActivity.java
+++ app/src/main/java/com/example/dialog/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.dialog;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.simpleAlertButton).setOnClickListener(v -> showAlertDialog());
-        findViewById(R.id.button2).setOnClickListener(v -> alert());
-        findViewById(R.id.button3).setOnClickListener(v -> alert2());
-        findViewById(R.id.button4).setOnClickListener(v -> alert3());
-        findViewById(R.id.button5).setOnClickListener(v -> alert4());
+        findViewById(R.id.listAlert).setOnClickListener(v -> showListAlert());
+        findViewById(R.id.datePickerAlert).setOnClickListener(v -> showDatePickerDialog());
+        findViewById(R.id.timePickerAlert).setOnClickListener(v -> showTimeAlertDialog());
+        findViewById(R.id.customAlert).setOnClickListener(v -> showCustomAlertDialog());
     }
 
     private void showAlertDialog(){
@@ -41,18 +49,58 @@ public class MainActivity extends AppCompatActivity {
         });
         builder.create().show();
     }
-    private void alert(){
+    private void showListAlert(){
         final String[] items = {"Option1", "Option2", "Option3", "Option4", "Option5", "Option6"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Choose option");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Clicked" + items[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
     }
-    private void alert2(){
+    private void showDatePickerDialog(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
 
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, "Chosen date: " + dayOfMonth + "/" + (month + 1) + "/" + year, Toast.LENGTH_SHORT).show();
+            }
+        }, year, month, day);
+        datePickerDialog.show();
     }
-    private void alert3(){
+    private void showTimeAlertDialog(){
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
 
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                Toast.makeText(MainActivity.this, "Time picked: " + hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
+            }
+        }, hour, minute, true);
+        timePickerDialog.show();
     }
-    private void alert4(){
+    private void showCustomAlertDialog(){
+        final android.app.Dialog dialog = new android.app.Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog);
 
+        Button btnSubmit = dialog.findViewById(R.id.btnSubmit);
+        EditText text = dialog.findViewById(R.id.etInput);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Input: " + text.getText().toString(), Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
